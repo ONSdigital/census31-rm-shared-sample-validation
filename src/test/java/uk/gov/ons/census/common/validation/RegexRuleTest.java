20eb0900b5110aad85985f7dd74cec90e9f2b754
Ravindra Tummuru <Ravindra.Tummuru@ext.ons.gov.uk>
2026-09-15 15:01:19 +0100

    CN 243 & CN 219 - Keep Jackson 2 annotation compatibility and verify with Jackson 3 round-trip tests.

--- src/test/java/uk/gov/ons/census/common/validation/RegexRuleTest.java
+++ src/test/java/uk/gov/ons/census/common/validation/RegexRuleTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 class RegexRuleTest {
   private static final String TEST_REGEX_UK_MOBILE_NUMBER = "^07[0-9]{9}$";
   private static final String TEST_REGEX_UK_MOBILE_NUMBER_ERROR = "Not a valid UK mobile number";
+  private static final String TEST_REGEX_INTERNATIONAL_PHONE_NUMBER = "^(\\+|00)?[1-9]\\d{1,14}$";
+  private static final String TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR =
+      "Not a valid international phone number";
 
   @Test
   void checkValidityValidPhoneNumber() {
@@ -43,5 +46,45 @@ class RegexRuleTest {
 
     Optional<String> actualResult = underTest.checkValidity("07123456789123456789");
     assertThat(actualResult).isPresent().contains(TEST_REGEX_UK_MOBILE_NUMBER_ERROR);
+  }
+
+  @Test
+  void checkValidityValidInternationalPhoneNumber() {
+    RegexRule underTest =
+        new RegexRule(
+            TEST_REGEX_INTERNATIONAL_PHONE_NUMBER, TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("+447123456789");
+    assertThat(actualResult).isNotPresent();
+  }
+
+  @Test
+  void checkValidityValidInternationalPhoneNumberWith00Prefix() {
+    RegexRule underTest =
+        new RegexRule(
+            TEST_REGEX_INTERNATIONAL_PHONE_NUMBER, TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("00447123456789");
+    assertThat(actualResult).isNotPresent();
+  }
+
+  @Test
+  void checkValidityInvalidInternationalPhoneNumberLeadingZeroCountryCode() {
+    RegexRule underTest =
+        new RegexRule(
+            TEST_REGEX_INTERNATIONAL_PHONE_NUMBER, TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("+0123456789");
+    assertThat(actualResult).isPresent().contains(TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
+  }
+
+  @Test
+  void checkValidityInvalidInternationalPhoneNumberContainsSeparators() {
+    RegexRule underTest =
+        new RegexRule(
+            TEST_REGEX_INTERNATIONAL_PHONE_NUMBER, TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("+44 7123 456789");
+    assertThat(actualResult).isPresent().contains(TEST_REGEX_INTERNATIONAL_PHONE_NUMBER_ERROR);
   }
 }
